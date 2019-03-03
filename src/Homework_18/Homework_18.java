9bf92f64d28a3ffba66727d61ed7f7aec951a7bb
Nadezhda Koliachko <nadezhda.kosareva95@gmail.com>
2019-03-03 22:36:06 +0300

    Add homewrok_18

---
+++ src/Homework_18/Homework_18.java
@@ -0,0 +1,11 @@
+package Homework_18;
+
+public class Homework_18 {
+    public static boolean containsHotels(String pattern, String content) {
+        return content.matches(pattern);
+    }
+
+    public static boolean moreThanNine(Double rate) {
+        return rate >= 9;
+    }
+}
